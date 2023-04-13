c621cdb461eb569d1c070dd9c024cfc545d6097c
omegamaster86 <masya868686@gmail.com>
2023-04-13 16:10:23 +0900

    props 導入

--- 04_react_basic/src/090_props/start/Example.js
+++ 04_react_basic/src/090_props/start/Example.js
@@ -1,12 +1,22 @@
 import Child from "./components/Child";
 
 const Example = () => {
-    return (
-        <>
-            <Child color=""/>
-            <Child color="red"/>
-        </>
-    )
-}
+  const hello = (arg) =>`Hello ${arg}`;
+  const o = {
+    color: "red",
+    num: 123
+  }
+  return (
+    <>
+      <Child 
+        {...o}
+        fn={hello}
+        bool
+        obj={{ name: "Tom", age: 18 }}
+      />
+      {/* <Child color="red"/> */}
+    </>
+  );
+};
 
 export default Example;
