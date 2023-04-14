8f08d3bc9e9729c4dbaab6e84335139daf9f1486
omegamaster86 <masya868686@gmail.com>
2023-04-14 20:08:23 +0900

    useStateについて

--- 05_state_and_event/src/020_useState/start/Example.js
+++ 05_state_and_event/src/020_useState/start/Example.js
@@ -1,8 +1,16 @@
-const Example = () => {
+import { useState } from "react"
 
+const Example = () => {
+  let [val, setVal] = useState();
+  // 分割代入を用いて使用する
   return (
     <>
-      
+      <input
+      type="text"
+      onChange={(e) => {
+        // const setFn = valArry[1];
+        setVal( e.target.value)
+      }}/> = {val}
     </>
   );
 };
