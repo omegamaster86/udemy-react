17619ad64e38fa9e3d553603a4e44cd3586db8c2
omegamaster86 <masya868686@gmail.com>
2023-04-15 19:57:28 +0900

    配列のステート

--- 05_state_and_event/src/064_state_array/start/Example.js
+++ 05_state_and_event/src/064_state_array/start/Example.js
@@ -1,9 +1,19 @@
-const Example = () => {
-  const numArray = [1, 2, 3, 4, 5];
+import { useState } from "react";
 
+// POINT 配列のstateの扱い方
+const Example = () => {
+  const [nums, setNums] = useState([1, 2, 3, 4, 5]);
+
+  const shuffle = () => {
+    const newNums = [ ...nums ];
+    const lastVal = newNums.pop();
+    newNums.unshift(lastVal);
+    setNums(newNums);
+  }
   return (
     <>
-      
+      <h1>{nums}</h1>
+      <button onClick={shuffle}>shuffle</button>
     </>
   );
 };
