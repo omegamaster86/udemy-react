a2bd529ef57ad589810c86f937aca9cc4a790721
omegamaster86 <masya868686@gmail.com>
2023-04-16 14:00:59 +0900

    練習問題クリア

--- 06_control_and_form/src/030_practice_list/start/Example.js
+++ 06_control_and_form/src/030_practice_list/start/Example.js
@@ -1,3 +1,23 @@
+import Profile from "./components/Profile";
+
+const persons = [
+  {
+    name: "Geo",
+    age: 18,
+    hobbies: ["sports", "music"],
+  },
+  {
+    name: "Tom",
+    age: 25,
+    hobbies: ["movie", "music"],
+  },
+  {
+    name: "Lisa",
+    age: 21,
+    hobbies: ["sports", "travel", "game"],
+  },
+];
+
 const Example = () => {
   return (
     <>
@@ -5,7 +25,13 @@ const Example = () => {
       <p>Profileコンポーネントを使用して、完成コードと同じ画面を作成してください。</p>
       <p>また、Profileコンポーネント内のリスト表示部分にkeyを設定して、ワーニング表示がされないようにしてください。</p>
       <ul>
-        {/* ここに記述 */}
+          {/* mapで各要素に特定の処理を行ったものを新しい配列とする */}
+          {persons.map((person) => (
+          /* リストにはkeyを設定することを忘れないように！ */
+          <li key={person.name}>
+            <Profile {...person} />
+          </li>
+        ))}
       </ul>
     </>
   );
